f7ea02ec66d8d7bade022efd4c0734ba7c3c35f5
subhrajyotipec <subhrajyotidas.ait@gmail.com>
2017-12-09 14:55:46 +0530

    lesson 2

---
+++ app/src/main/java/com/subhra/swooch/Activity_Legue.kt
@@ -0,0 +1,12 @@
+package com.subhra.swooch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Legue : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__legue)
+    }
+}
