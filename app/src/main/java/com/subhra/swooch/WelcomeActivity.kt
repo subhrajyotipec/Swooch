303a5f0cc56bcf7eabc00f942540d817e55150ea
subhrajyotipec <subhrajyotidas.ait@gmail.com>
2017-12-09 15:43:06 +0530

    lesson 3

--- app/src/main/java/com/subhra/swooch/WelcomeActivity.kt
+++ app/src/main/java/com/subhra/swooch/WelcomeActivity.kt
@@ -2,11 +2,17 @@ package com.subhra.swooch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val legueintent= intent(this, Activity_Legue::class.java)
+            startActivity(legueintent)
+        }
     }
 }
